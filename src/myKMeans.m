e82300482acdf7e4d8bc1abfa2b1b71e182ee0f8
zhhsplendid <zhhy409923957@163.com>
2015-10-04 00:24:30 -0500

    Now myKMeans is correct
    rt

--- src/myKMeans.m
+++ src/myKMeans.m
@@ -1,37 +1,61 @@
-function [centers, labels] = myKMeans(inputData, k)
+function [labels, centers] = myKMeans(inputData, k)
+
+% A kmeans function uses Euclidean distance and ignore empty cluster error.
+% The main idea combines three steps: compute distances -> compare labels
+% (whether points are assigned to same centers as previous iteration) -> 
+% new labels, new centers.
+
     [numData, dimData] = size(inputData);
+    labels = zeros(numData, 1);
     
-    centers = rand(k, dim_data) .* repmat(max(input_data, [], 1), k, 1);
-    labels = zeros(1, num_data);
+    MAX_ITERATION = 256; %If kmeans exceeds iteration limitation, we return. 
     
-    MAX_ITERATION = 2^30;
+    %Centers are allocated randly in the data space.
+    %But don't exceed range of inputData.
+    Xmins = min(inputData,[],1);
+    Xmaxs = max(inputData,[],1);
+    centers =  unifrnd(Xmins(ones(k,1),:), Xmaxs(ones(k,1),:));;
     
     for iteration = 1:MAX_ITERATION
-        dataSquare = repmat(sum((inputData .* inputData), 2)', k, 1);
-        centerSquare = repmat(sum((centers .* centers), 2), 1, numData);
-        dataCenterProduct = centers * inputData';
-        distances = dataSquare - 2*dataCenterProduct + centerSquare;
+        %iteration
         
-        [maxValues, newLabels] = max(distances, [], 1);
+        % Compute distances by Euclidean distance (L2-norm)
+        % distances is a numData-by-k matrix, the (i, j) entry means the
+        % distance from point i to center j. 
+        dataSquare = repmat(sum((inputData .* inputData), 2), 1, k);
+        centerSquare = repmat(sum((centers .* centers), 2)', numData, 1);
+        dataCenterProduct = inputData * centers';
+        distances = dataSquare - 2 * dataCenterProduct + centerSquare;
+        
+        %Get labels 
+        [minValues, newLabels] = min(distances, [], 2);
+        
+        % Compare labels to previous labels, if labels don't change, we can
+        % finish kmeans iteration.
         if newLabels == labels
             break;
         end
         
         labels = newLabels;
         
+        %Update new centers
         for i = 1:k
-            points = inputData(newLabels == i, :);
+            %those points are assigned to center i
+            points = inputData(newLabels == i, :); 
             if size(points, 1) ~= 0
                 centers(i, :) = sum(points, 1) / size(points, 1);
+            else
+                iteration
+                'warning, emtpy cluster'
+                % Until now we don't deal with empty clusters.
+                % There are two methods:
+                % 1. see it as fault or warning
+                % 2. assgin this center a position of a point which is
+                % the most far to its center.
             end
         end
     end
     
-    for i = 1:k
-        points = inputData(labels == i, :);
-        if size(points, 1) ~= 0
-            centers(i, :) = sum(points, 1) / size(points, 1);
-        end
-    end
+    
 end
 
